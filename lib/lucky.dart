4b4ca9aa497b6e5033c6652355e75a90e639cad0
hum00058 <hum00058@algonquinlive.com>
2024-09-24 16:35:22 -0400

    move random init inside Lucky constructor

--- lib/lucky.dart
+++ lib/lucky.dart
@@ -1,8 +1,6 @@
 import 'dart:math';
 import 'dart:io';
 
-final random = Random();
-
 class Lucky {
   late int number;
 
@@ -11,6 +9,7 @@ class Lucky {
     final min = int.parse(envVars['MIN'] ?? '0');
     final max = int.parse(envVars['MAX'] ?? '100');
 
+    final random = Random();
     number = min + random.nextInt(max - min + 1);
   }
 }
